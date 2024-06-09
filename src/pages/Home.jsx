e994ebb7ed3b430b5f6a7ba9fc5c8b33b4bd1535
joao costa <joao.pedro.scosta2006@gmail.com>
2024-06-09 23:46:23 +0000

    style: estilização do h1 e do p do card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,8 @@ export function Home() {
         <IconContainer>
         <img src={iconStarImg} alt="ícone de uma estrela" />
         </IconContainer>
-
         <h1>Como foi o atendimento?</h1>
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecmos muito seu feedback para podermos melhorar nosso atendimento</p>
-
         <ButtonContainer>
             <button>1</button>
             <button>2</button>
@@ -19,7 +17,6 @@ export function Home() {
             <button>4</button>
             <button>5</button>
         </ButtonContainer>
-
         <button>Enviar</button>
     </CardContainer>
   )
